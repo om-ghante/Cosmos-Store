3c4e8909ef4f81001946aa6c106d40515577adc4
om-ghante <omghante.official@gmail.com>
2024-08-31 23:37:51 +0530

    Another Commit

---
+++ pro/hackathon-storefront/src/modules/home/components/hero/index.tsx
@@ -0,0 +1,14 @@
+import { Github } from "@medusajs/icons"
+import { Button, Heading } from "@medusajs/ui"
+
+const Hero = () => {
+  return (
+    <div className="h-[75vh] w-full border-b border-ui-border-base relative bg-ui-bg-subtle">
+      <div className="absolute inset-0 z-10 flex flex-col justify-center items-center text-center small:p-32 gap-6">
+          
+      </div>
+    </div>
+  )
+}
+
+export default Hero
